c3e28fad7e7701a882d79d43f8d0227074e0f181
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-06-02 19:09:07 +0530

    It somewhat works now

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get("/auth/google/callback", passport.authenticate("google", {
+router.get("/google/callback", passport.authenticate("google", {
     successRedirect: "https://smarter-receipt.vercel.app",
     failureRedirect: "https://smarter-receipt.vercel.app/login"
 }))
